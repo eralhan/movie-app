cdf64249c9168fdda9aa41467fec281356d343c2
eralhan <yessenyeralkhan@gmail.com>
2023-12-16 05:32:06 +0600

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./component/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/component/Header.jsx
@@ -0,0 +1,50 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>Movie App</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavDropdown
+                id="movies-nav-dropdown"
+                title="Movies"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item href="#">Popular</NavDropdown.Item>
+                <NavDropdown.Item href="#">Now Playing</NavDropdown.Item>
+                <NavDropdown.Item href="#">Upcoming</NavDropdown.Item>
+                <NavDropdown.Item href="#">Top Rated</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                id="tv-series-nav-dropdown"
+                title="TV Series"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item href="#">Popular</NavDropdown.Item>
+                <NavDropdown.Item href="#">Airing Today</NavDropdown.Item>
+                <NavDropdown.Item href="#">On The Air</NavDropdown.Item>
+                <NavDropdown.Item href="#">Top Rated</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                id="people-nav-dropdown"
+                title="People"
+                menuVariant="dark"
+              >
+                <NavDropdown.Item href="#">Popular people</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
